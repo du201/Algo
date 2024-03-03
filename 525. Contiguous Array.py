2b46ae4806f12cba77b5c4338c0c58e8e057e90e
Xin Du <54476661+du201@users.noreply.github.com>
2024-03-03 15:56:58 -0800

    day 46

---
+++ 525. Contiguous Array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        d = {}
+        d[0] = -1 # Initialize Zero_Running_Sum with -1 Index
+
+        max_len = 0
+        running_sum = 0
+
+        for i in range(len(nums)):
+            running_sum += nums[i] if nums[i] == 1 else -1
+
+            if running_sum in d:
+                max_len = max(max_len, i - d[running_sum])
+
+            else: 
+                d[running_sum] = i
+
+        return max_len
--- Sliding Window.md
+++ Sliding Window.md
@@ -4,4 +4,4 @@ Example 1: You are given a string s and an integer k. Find the length of the lon
 
 For example, given s = "eceba" and k = 2, return 3. The longest substring with at most 2 distinct characters is "ece".
 
-However, for 
+However, for questions that don't satisfy this constraint i.e. N. 560 (Subarray sum equals K), we can't use sliding windows. A common solution is to use prefix sum + hashmap count
